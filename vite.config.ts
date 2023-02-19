684693e30d821bb08d5c610b626aefce767e664d
Алексей Астротенко <aastrotenko@mail.ru>
2023-02-19 21:29:05 +0700

    initialization

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/radio',
   plugins: [
     vue(),
     tsconfigPaths(),
